f3a5f658abd6be442d83f778a7f8864e4be6cc7f
lipohub <142733086+lipohub@users.noreply.github.com>
2025-11-27 23:51:33 +0300

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,15 @@
+# handlers/start.py
+from utils import main_menu
+
+def register_handlers(bot):
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        bot.send_message(message.chat.id,
+            "Привет! Это поисковик учеников нашей школы\n\n"
+            "Тут можно найти инфу о человеке или оставить заявку на добавление/исправление данных.\n\n"
+            "Доступные команды:\n/start • /search • /add_tip • /admin • /help",
+            reply_markup=main_menu())
+
+    @bot.message_handler(commands=['help'])
+    def help_command(message):
+        bot.send_message(message.chat.id, "Доступные команды:\n/start — главное меню\n/search — поиск\n/add_tip — дать наводку\n/admin — админка")
