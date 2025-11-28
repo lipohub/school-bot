86cc43a5dec33b36693585abce5227f60becc661
lipohub <142733086+lipohub@users.noreply.github.com>
2025-11-28 17:03:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 # main.py
 import telebot
 from config import BOT_TOKEN
-from database import load_db, save_db, generate_key
-from handlers import register_all_handlers
+from database import load_db, save_db, generate_key, db
 
 bot = telebot.TeleBot(BOT_TOKEN)
-bot.db = load_db()
-bot.save_db = save_db
-bot.generate_key = generate_key
+bot.db = db
 
 # Регистрируем все обработчики
+from handlers import register_all_handlers
 register_all_handlers(bot)
 
 if __name__ == '__main__':
-    print("Бот запущен!")
+    print("Бот запущен! Структура — огонь!")
     bot.infinity_polling()
