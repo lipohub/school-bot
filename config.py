cc1008c81e11be368516ec276414f4f6b1c6a1ff
lipohub <142733086+lipohub@users.noreply.github.com>
2025-11-28 16:51:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+# config.py
 import os
 
-BOT_TOKEN = "8483130885:AAEBgryQXbUnNUuS22ZJeUdQVOo4Jua6Vx0"
-ADMIN_IDS = [1967855685]  # ← сюда можно добавить ещё админов через запятую
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = [1967855685]  # Список ID администраторов
 
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
-    raise ValueError("Ошибка: MONGO_URI не найден! Зайди в Render → Environment → добавь переменную MONGO_URI")
+    raise ValueError("Ошибка: переменная MONGO_URI не установлена")
