85b61438b77a819cee4327df3eb4554320cb6185
lipohub <142733086+lipohub@users.noreply.github.com>
2025-11-28 17:13:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-# main.py
 import telebot
 from config import BOT_TOKEN
-from database import load_db, save_db, generate_key, db
+from database import load_db, save_db, generate_key
+from handlers import register_all_handlers
+from utils import main_menu, get_students_kb
 
 bot = telebot.TeleBot(BOT_TOKEN)
-bot.db = db
+bot.db = load_db()
+bot.save_db = save_db
+bot.generate_key = generate_key
+bot.main_menu = main_menu
+bot.get_students_kb = get_students_kb
 
 # Регистрируем все обработчики
-from handlers import register_all_handlers
 register_all_handlers(bot)
 
 if __name__ == '__main__':
