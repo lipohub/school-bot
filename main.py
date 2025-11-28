23860a41af9a4ce108bdb51d08abea04c316f016
lipohub <142733086+lipohub@users.noreply.github.com>
2025-11-28 16:52:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 # main.py
 import telebot
-from config import BOT_TOKEN, ADMIN_IDS
-from database import load_db, save_db
+from config import BOT_TOKEN
+from database import load_db, save_db, generate_key
 from handlers import register_all_handlers
 
-# Создаем экземпляр бота
 bot = telebot.TeleBot(BOT_TOKEN)
-
-# Загружаем данные из MongoDB при запуске
 bot.db = load_db()
-
-# Добавляем необходимые атрибуты к объекту бота
-bot.config = type('Config', (), {'ADMIN_IDS': ADMIN_IDS})()
-bot.save_db = save_db  # Функция для сохранения данных в базу
+bot.save_db = save_db
+bot.generate_key = generate_key
 
 # Регистрируем все обработчики
 register_all_handlers(bot)
 
 if __name__ == '__main__':
     print("Бот запущен!")
-    print(f"Загружено записей из базы данных: {len(bot.db)}")
     bot.infinity_polling()
